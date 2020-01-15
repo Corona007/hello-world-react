e0ecab8521d4da7a5d86a3c5b466ffa107d06681
Esteban <eca121096@gmail.com>
2020-01-15 14:29:45 -0800

    initial commit

---
+++ src/components/HelloComp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HelloComp extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
+export default HelloComp;
